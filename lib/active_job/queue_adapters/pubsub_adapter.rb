cd14a61eab0673833210d8d1d640de72a1d0db29
Jusung Park <56177373+JusungPark6@users.noreply.github.com>
2023-09-04 17:20:29 -0400

    Revert "Jusung Park Kisi API Junior Developer Coding Challenge"

--- lib/active_job/queue_adapters/pubsub_adapter.rb
+++ lib/active_job/queue_adapters/pubsub_adapter.rb
@@ -3,15 +3,11 @@
 module ActiveJob
   module QueueAdapters
     class PubsubAdapter
-      attr_reader :pubsub
       # Enqueue a job to be performed.
       #
       # @param [ActiveJob::Base] job The job to be performed.
       def enqueue(job)
-        pubsub = Pubsub.new
-        topic = pubsub.topic("job_data_sub")
-        serialized_job = job.serialize
-        message = topic.publish(serialized_job.to_json)
+        raise(NotImplementedError)
       end
 
       # Enqueue a job to be performed at a certain time.
@@ -19,8 +15,7 @@ module ActiveJob
       # @param [ActiveJob::Base] job The job to be performed.
       # @param [Float] timestamp The time to perform the job.
       def enqueue_at(job, timestamp)
-        job.scheduled_at = timestamp
-        enqueue(job)
+        raise(NotImplementedError)
       end
     end
   end
